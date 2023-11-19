3861e629c6528272a3a3445ad7c72cd99461eb44
mistidevs-alx <mistidevs@gmail.com>
2023-11-19 07:11:51 -0800

    Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,5 +18,12 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
+/***** DEFAULT FUNCTIONS ******/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/****** SORTING ALGORITHMS ****/
+void bubble_sort(int *array, size_t size);
 
 #endif
