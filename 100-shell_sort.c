cbf88cbf666b379bcc7261f7abd99963e8d01388
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-26 05:18:34 +0000

    Shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort - function that implement an a quick form
+ *		of insertiong sort
+ * @array: the array list ot item
+ * @size: the size of the array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, j, i;
+	int temp;
+
+	while (gap <= size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = temp;
+
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
+}
